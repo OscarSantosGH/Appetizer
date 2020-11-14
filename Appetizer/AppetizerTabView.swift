c39ece9ddc2879d4ba11e7ec3196518ca2808d3c
Oscar Santos <oscarsantos.go@gmail.com>
2020-11-13 19:17:21 -0500

    added: AppetizerTabView

--- Appetizer/AppetizerTabView.swift
+++ Appetizer/AppetizerTabView.swift
@@ -7,15 +7,18 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct AppetizerTabView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView{
+            
+            Text("Hello, world!")
+                .padding()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        AppetizerTabView()
     }
 }
