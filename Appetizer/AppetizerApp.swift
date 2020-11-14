bccd3ddf3e8ffe63dd7fa381e935879f2edb930d
Oscar Santos <oscarsantos.go@gmail.com>
2020-11-13 19:10:03 -0500

    Initial Commit

---
+++ Appetizer/AppetizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizerApp.swift
+//  Appetizer
+//
+//  Created by Oscar Santos on 11/13/20.
+//
+
+import SwiftUI
+
+@main
+struct AppetizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Appetizer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Appetizer
+//
+//  Created by Oscar Santos on 11/13/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
