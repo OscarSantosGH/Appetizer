da7e00983ab186c764d354838fe73945ae9ea5a1
Oscar Santos <oscarsantos.go@gmail.com>
2020-11-26 10:07:04 -0500

    added: a List view with mock data of the appetizer cells

--- Appetizer/AppetizerListView.swift
+++ Appetizer/AppetizerListView.swift
@@ -10,8 +10,10 @@ import SwiftUI
 struct AppetizerListView: View {
     var body: some View {
         NavigationView{
-            Text("Appetizer List View")
-                .navigationTitle("🍔 Appetizer")
+            List(MockData.appetizers){ appetizer in
+                AppetizerListCell(appetizer: appetizer)
+            }
+            .navigationTitle("🍔 Appetizer")
         }
     }
 }
