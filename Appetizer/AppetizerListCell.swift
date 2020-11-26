80f1637a450678ebb9e2a42eab8be3ab2fab6ee1
Oscar Santos <oscarsantos.go@gmail.com>
2020-11-26 10:05:03 -0500

    added: AppetizerListCell

---
+++ Appetizer/AppetizerListCell.swift
@@ -0,0 +1,36 @@
+//
+//  AppetizerListCell.swift
+//  Appetizer
+//
+//  Created by Oscar Santos on 11/26/20.
+//
+
+import SwiftUI
+
+struct AppetizerListCell: View {
+    let appetizer: Appetizer
+    var body: some View {
+        HStack{
+            Image("asian-flank-steak")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 120, height: 90)
+                .cornerRadius(8)
+            VStack(alignment: .leading, spacing: 5){
+                Text(appetizer.name)
+                    .font(.title2)
+                    .fontWeight(.medium)
+                Text("$\(appetizer.price, specifier: "%.2f")")
+                    .foregroundColor(.secondary)
+                    .fontWeight(.semibold)
+            }
+            .padding(.leading)
+        }
+    }
+}
+
+struct AppetizerListCell_Previews: PreviewProvider {
+    static var previews: some View {
+        AppetizerListCell(appetizer: MockData.sampleAppetizer)
+    }
+}
