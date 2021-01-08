65baaf8dd2cc20ca3427c7e3cc68741b4ad6dee6
Oscar Santos <oscarsantos.go@gmail.com>
2021-01-08 06:37:15 -0500

    added: APError enum to handle Appetizer API network errors

---
+++ Appetizer/APError.swift
@@ -0,0 +1,15 @@
+//
+//  APError.swift
+//  Appetizer
+//
+//  Created by Oscar Santos on 1/8/21.
+//
+
+import Foundation
+
+enum APError: Error{
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
