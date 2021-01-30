a57b1353fd6bcc6e6de8b04739976884e3c7554e
Oscar Santos <oscarsantos.go@gmail.com>
2021-01-30 18:14:41 -0500

    added: getAppetizers function that make a network calls to get the appetizers

--- Appetizer/AppetizerListView.swift
+++ Appetizer/AppetizerListView.swift
@@ -8,12 +8,32 @@
 import SwiftUI
 
 struct AppetizerListView: View {
+    
+    @State var appetizers: [Appetizer] = []
+    
     var body: some View {
         NavigationView{
-            List(MockData.appetizers){ appetizer in
+            List(appetizers){ appetizer in
                 AppetizerListCell(appetizer: appetizer)
             }
             .navigationTitle("🍔 Appetizer")
+        }
+        .onAppear(){
+            getAppetizers()
+        }
+    }
+    
+    func getAppetizers(){
+        NetworkManager.shared.getAppetizers { result in
+            DispatchQueue.main.async {
+                switch result{
+                case .success(let appetizers):
+                    self.appetizers = appetizers
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+            
         }
     }
 }
