8301505140d8d64c36ff046776f15948b5954bb2
Oscar Santos <oscarsantos.go@gmail.com>
2021-01-31 11:28:36 -0500

    mod: AppetizersListViewModel to include alertItem to parse the possible error that can throw the network call and publish that alertItem

--- Appetizer/AppetizersListViewModel.swift
+++ Appetizer/AppetizersListViewModel.swift
@@ -10,15 +10,25 @@ import SwiftUI
 final class AppetizersListViewModel: ObservableObject {
     
     @Published var appetizers: [Appetizer] = []
+    @Published var alertItem: AlertItem?
     
     func getAppetizers(){
-        NetworkManager.shared.getAppetizers { result in
+        NetworkManager.shared.getAppetizers { [self] result in
             DispatchQueue.main.async {
                 switch result{
                 case .success(let appetizers):
                     self.appetizers = appetizers
                 case .failure(let error):
-                    print(error.localizedDescription)
+                    switch error {
+                    case .invalidData:
+                        alertItem = AlertContext.invalidData
+                    case .invalidURL:
+                        alertItem = AlertContext.invalidURL
+                    case .invalidResponse:
+                        alertItem = AlertContext.invalidResponse
+                    case .unableToComplete:
+                        alertItem = AlertContext.unableToComplete
+                    }
                 }
             }
             
