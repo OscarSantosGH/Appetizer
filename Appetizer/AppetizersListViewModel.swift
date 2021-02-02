b382f97940bea1af206c66ab83eee46df939dec8
Oscar Santos <oscarsantos.go@gmail.com>
2021-02-02 18:50:58 -0500

    added: isLoading var that is set true when the function getAppetizer is called and is set to false when the network call finished.

--- Appetizer/AppetizersListViewModel.swift
+++ Appetizer/AppetizersListViewModel.swift
@@ -11,10 +11,13 @@ final class AppetizersListViewModel: ObservableObject {
     
     @Published var appetizers: [Appetizer] = []
     @Published var alertItem: AlertItem?
+    @Published var isLoading: Bool = false
     
     func getAppetizers(){
+        isLoading = true
         NetworkManager.shared.getAppetizers { [self] result in
             DispatchQueue.main.async {
+                isLoading = false
                 switch result{
                 case .success(let appetizers):
                     self.appetizers = appetizers
