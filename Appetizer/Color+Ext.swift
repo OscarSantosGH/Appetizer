8d8daa829e381bfe45c239887325fd7c84f37c4e
Oscar Santos <oscarsantos.go@gmail.com>
2021-02-02 18:22:42 -0500

    Added: Color extension to put the brandPrimary color in a static constant

---
+++ Appetizer/Color+Ext.swift
@@ -0,0 +1,16 @@
+//
+//  Color+Ext.swift
+//  Appetizer
+//
+//  Created by Oscar Santos on 2/2/21.
+//
+
+import SwiftUI
+
+extension Color{
+    static let brandPrimary = Color("brandPrimary")
+}
+
+extension UIColor{
+    static let brandPrimary = UIColor(named: "brandPrimary")
+}
