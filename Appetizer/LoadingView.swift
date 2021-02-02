94337c8657f3a8223349d11a993b9a701a9ccb93
Oscar Santos <oscarsantos.go@gmail.com>
2021-02-02 18:36:41 -0500

    added: LoadingView with an activityIndicator UIViewRepresentable

---
+++ Appetizer/LoadingView.swift
@@ -0,0 +1,31 @@
+//
+//  LoadingView.swift
+//  Appetizer
+//
+//  Created by Oscar Santos on 2/2/21.
+//
+
+import SwiftUI
+
+struct ActivityIndicator: UIViewRepresentable {
+    func makeUIView(context: Context) -> UIActivityIndicatorView {
+        let activityIndicatorView = UIActivityIndicatorView(style: .large)
+        activityIndicatorView.color = UIColor.brandPrimary
+        activityIndicatorView.startAnimating()
+        return activityIndicatorView
+    }
+    
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: Context) {}
+    
+}
+
+struct LoadingView: View {
+    var body: some View{
+        ZStack{
+            Color(.systemBackground)
+                .edgesIgnoringSafeArea(.all)
+            
+            ActivityIndicator()
+        }
+    }
+}
